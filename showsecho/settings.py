ffe5963e66a98e8e8d18b46ae83e405009c49cd0
easybulb <easybulb@gmail.com>
2024-10-30 10:51:26 +0000

    create database and migrate

--- showsecho/settings.py
+++ showsecho/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'showsecho.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
