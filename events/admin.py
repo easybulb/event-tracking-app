73f358eeeeca63d1f907d142c45c7b3b95dfe457
easybulb <easybulb@gmail.com>
2024-10-30 08:48:55 +0000

    apply migration and register models in events admin.py

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Event, Location, Category
 
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(Location)
+admin.site.register(Category)
+
